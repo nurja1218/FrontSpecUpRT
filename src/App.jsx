455325b0fdb324602c46e9dddbdd7bc5eef182d6
nurja1218 <nurja1218@gmail.com>
2021-09-30 10:35:31 +0900

    react & typescript frontend 구성을 위한 study start

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>start react project with typescript!</h1>;
+};
+ReactDOM.render(<App />, document.getElementById('app'));
